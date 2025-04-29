7a22d4f7814186ac09c28163d5f5983407aeef47
Grigory Morozov <morozehc@yandex.ru>
2025-04-29 13:45:54 +0300

    feat: preparing project0

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
-
+export const API_URL = `${process.env.API_ORIGIN}/v1/wff-cohort-26`;
 export const settings = {
-
+    headers:{
+      autohorization:`${process.env.API_TOKEN}`,
+      'Content-Type': 'application/json'
+    }
 };
